a2d595cd0c0ace16772a6903bcae4b2b567f46d5
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-10 15:11:22 +0530

    Created the functions that happen when increment and decrement counter buttons are pressed

--- app/src/main/java/com/lokesh58/counter/MainActivity.java
+++ app/src/main/java/com/lokesh58/counter/MainActivity.java
@@ -2,12 +2,33 @@ package com.lokesh58.counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView counterDisplay;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counterDisplay = (TextView) findViewById(R.id.counterDisplay);
+    }
+
+    public void increaseCounter(View view) {
+        counter++;
+        counterDisplay.setText(Integer.toString(counter));
+    }
+
+    public void decreaseCounter(View view) {
+        counter--;
+        if (counter < 0) {
+            counter = 0;
+            Toast.makeText(this, "Counter already at 0!", Toast.LENGTH_SHORT).show();
+        }
+        counterDisplay.setText(Integer.toString(counter));
     }
 }
